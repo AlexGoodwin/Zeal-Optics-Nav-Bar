d5aa5a0ea3adc24d8a836b1281412c28c68598a9
Alex Goodwin <agoodwin727@gmail.com>
2016-05-12 10:46:08 -0600

    Removed jQuery animation

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
+/*jslint node:true */
+
+'use strict';
+
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    input = './css/*.scss',
+    input = './css/style.css',
     output = './css';
 
 gulp.task('default', function () {
     return gulp.src(input)
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['last 3 versions'],
             cascade: false
         }))
-        .pipe(sass())
         .pipe(gulp.dest(output));
 });
